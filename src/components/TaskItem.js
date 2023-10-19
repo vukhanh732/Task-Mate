af1863421ce02149a6095ae79ba39ccc8a995a37
vukhanh732 <vukhanh732@DESKTOP-HVJUMOE>
2023-10-19 14:14:05 +1100

    Mark task as complete

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { TaskContext } from '../TaskContext';
 
 function TaskItem({ task }) {
+  const [tasks, setTasks] = useContext(TaskContext);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(task.title);
+
+  const toggleCompletion = () => {
+    setTasks(tasks.map(t => {
+      if (t.id === task.id) {
+        return { ...t, completed: !t.completed };
+      }
+      return t;
+    }));
+  };
+
+  const saveEdit = () => {
+    setTasks(tasks.map(t => {
+      if (t.id === task.id) {
+        return { ...t, title: editedTitle };
+      }
+      return t;
+    }));
+    setIsEditing(false);
+  };
+
   return (
-    <div className="task-item">
-      <input type="checkbox" checked={task.completed} onChange={() => {}} /> {/* onChange to be implemented */}
-      <span>{task.title}</span>
-      {/* Future: Edit and Delete buttons/options */}
+    <div className={`task-item ${task.completed ? 'completed' : ''}`}>
+      <input type="checkbox" checked={task.completed} onChange={toggleCompletion} />
+      
+      {
+        isEditing ? 
+        <input 
+          type="text" 
+          value={editedTitle} 
+          onChange={e => setEditedTitle(e.target.value)}
+          onBlur={saveEdit}
+          onKeyPress={event => {
+            if (event.key === "Enter") {
+              saveEdit();
+            }
+          }}
+        /> 
+        : 
+        <span onDoubleClick={() => setIsEditing(true)}>
+          {task.title}
+        </span>
+      }
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -5,9 +5,11 @@ import TaskItem from './TaskItem';
 function TaskList() {
   const [tasks, setTasks] = useContext(TaskContext);
 
+  const sortedTasks = tasks.sort((a, b) => (a.completed === b.completed ? 0 : a.completed ? 1 : -1));
+
   return (
     <div className="task-list">
-      {tasks.map(task => (
+      {sortedTasks.map(task => (
         <TaskItem key={task.id} task={task} />
       ))}
     </div>
